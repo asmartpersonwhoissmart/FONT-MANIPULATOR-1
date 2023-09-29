80f308ebd27a5eb81a4ffc03924231ed78ab5f9c
Ishan Bhat <124944324+asmartpersonwhoissmart@users.noreply.github.com>
2023-09-28 18:40:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+leftWrist_x = 0;
+rightWrist_x = 0;
+difference = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
+
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
+}
+
+function draw(){
+    background("#5196e3");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#FF2D00");
+    text('Ishan',50,400);
+}
+
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist_x - rightWrist_x);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
+}
